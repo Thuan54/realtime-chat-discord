5f9f47e97dfe2b8f489f7b0abf0b05461a5e5361
Thuan <th0954290@gmal.com>
2026-09-17 16:26:03 +0700

    setup backend

---
+++ backend/src/server.ts
@@ -0,0 +1,16 @@
+import express from "express"
+
+const app = express()
+
+app.get('/', (req,res) => {
+    res.send("Hello")
+})
+
+const port = 3000
+
+app.listen(port, err => {
+    if (err) {
+        return console.error(err);
+    }
+    return console.log(`server is listening on ${port}`);
+})
